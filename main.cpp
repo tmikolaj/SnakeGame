790482df312083e1e52040d0cd1497b2169cd189
mikolaj <mikolajt55@gmail.com>
2025-04-15 18:43:28 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(512, 512), "Snake Game", sf::Style::Close | sf::Style::Resize);
+
+    while (window.isOpen()) {
+        sf::Event evnt;
+        while (window.pollEvent(evnt)) {
+            switch (evnt.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::Resized:
+                    std::cout << evnt.size.width << ' ' << evnt.size.height << '\n';
+            }
+
+        }
+
+        // Clear the window with black color
+        window.clear(sf::Color::Black);
+
+        // Display the current frame
+        window.display();
+    }
+
+    return 0;
+}
+
