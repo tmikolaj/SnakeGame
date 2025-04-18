42ecf4e9ee3e15ba507c1ca93911c8a657bd5317
mikolaj <mikolajt55@gmail.com>
2025-04-18 11:36:17 +0200

    Added asset manager to manage fonts

---
+++ include/AssetManager.h
@@ -0,0 +1,25 @@
+//
+// Created by mikolaj on 4/17/25.
+//
+#pragma once
+
+#include <map>
+#include <memory>
+#include <string>
+#include <SFML/Graphics/Font.hpp>
+
+namespace Engine
+{
+    class AssetManager
+    {
+    public:
+        AssetManager();
+        ~AssetManager();
+
+        void addFont(int id, const std::string& filePath);
+        const sf::Font& getFont(int id) const;
+
+    private:
+        std::map<int, std::unique_ptr<sf::Font>> fonts;
+    };
+}
---
+++ src/AssetManager.cpp
@@ -0,0 +1,34 @@
+//
+// Created by mikolaj on 4/17/25.
+//
+
+#include <iostream>
+#include "../include/AssetManager.h"
+
+Engine::AssetManager::AssetManager() {
+
+}
+Engine::AssetManager::~AssetManager() {
+
+}
+void Engine::AssetManager::addFont(int id, const std::string &filePath) {
+    auto font = std::make_unique<sf::Font>();
+
+    if (font->loadFromFile(filePath)) {
+        std::cout << "Font loaded successfully\n";
+        fonts[id] = std::move(font);
+    } else {
+        std::cerr << "Failed to load font: " << filePath << '\n';
+    }
+
+}
+const sf::Font& Engine::AssetManager::getFont(int id) const {
+        auto it = fonts.find(id);
+        if (it != fonts.end()) {
+            return *(it->second);
+        } else {
+            std::cerr << "Font ID not found: " << id << "\n";
+            exit(1);
+    }
+
+}
