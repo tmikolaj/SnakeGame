41e0436fefa1e1a7d867cbfbc359be9ea535a2ab
mikolaj <mikolajt55@gmail.com>
2025-04-18 12:10:26 +0200

    Added play and exit buttons (not yet working)

--- include/MainMenu.h
+++ include/MainMenu.h
@@ -13,6 +13,15 @@ class MainMenu : public Engine::State {
 private:
     std::shared_ptr<Context> context;
     sf::Text gameTitle;
+
+    sf::Text playButton;
+    sf::Text exitButton;
+
+    bool isPlayButtonSelected;
+    bool isPlayButtonPressed;
+
+    bool isExitButtonSelected;
+    bool isExitButtonPressed;
 public:
     MainMenu(std::shared_ptr<Context>& context);
     ~MainMenu();
--- src/MainMenu.cpp
+++ src/MainMenu.cpp
@@ -2,22 +2,37 @@
 // Created by mikolaj on 4/17/25.
 //
 
-#include <iostream>
 #include <SFML/Window/Event.hpp>
 #include "../include/MainMenu.h"
 
-MainMenu::MainMenu(std::shared_ptr<Context>& context) : context(context) {
+MainMenu::MainMenu(std::shared_ptr<Context>& context) : context(context), isPlayButtonSelected(true), isPlayButtonPressed(false), isExitButtonSelected(false), isExitButtonPressed(false) {
 
 }
 MainMenu::~MainMenu() {
 
 }
 void MainMenu::init() {
-    std::cout << "Pointer check: context = " << context.get() << ", assets = " << context->assets.get() << "\n";
-
     context->assets->addFont(MAIN_FONT, "assets/Roboto-Italic-VariableFont_wdth,wght.ttf");
+
+    // Title init
     gameTitle.setFont(context->assets->getFont(MAIN_FONT));
     gameTitle.setString("Snake Game");
+    gameTitle.setOrigin(gameTitle.getLocalBounds().width/2,gameTitle.getLocalBounds().height/2);
+    gameTitle.setPosition(context->window->getSize().x / 2, context->window->getSize().y / 2 - 150);
+
+    // Buttons
+    // Play button
+    playButton.setFont(context->assets->getFont(MAIN_FONT));
+    playButton.setString("PLAY");
+    playButton.setOrigin(playButton.getLocalBounds().width/2,playButton.getLocalBounds().height/2);
+    playButton.setPosition(context->window->getSize().x / 2, context->window->getSize().y / 2 - 25);
+    playButton.setCharacterSize(20);
+    // Exit button
+    exitButton.setFont(context->assets->getFont(MAIN_FONT));
+    exitButton.setString("EXIT");
+    exitButton.setOrigin(exitButton.getLocalBounds().width/2,exitButton.getLocalBounds().height/2);
+    exitButton.setPosition(context->window->getSize().x / 2, context->window->getSize().y / 2 + 25);
+    exitButton.setCharacterSize(20);
 }
 void MainMenu::processInput() {
     sf::Event evnt;
@@ -33,5 +48,7 @@ void MainMenu::update(sf::Time deltaTime) {
 void MainMenu::draw() {
     context->window->clear();
     context->window->draw(gameTitle);
+    context->window->draw(playButton);
+    context->window->draw(exitButton);
     context->window->display();
 }
